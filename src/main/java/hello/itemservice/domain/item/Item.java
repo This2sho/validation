f6ce845158f24f5f98d063ca5edf90cda0fad31e
This2sho <zx00018@naver.com>
2022-05-22 16:52:07 +0900

    Refactor: BeanValidation 분리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -14,15 +14,15 @@ public class Item {
 
     private Long id;
 
-    @NotBlank(groups = UpdateCheck.class) //수정 요구사항 추가
+//    @NotBlank(groups = UpdateCheck.class) //수정 요구사항 추가
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min=1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min=1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
